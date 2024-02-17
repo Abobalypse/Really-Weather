f19b204e492f8048dd15f968dc5a7a2b8d46cfd0
abobalypse <abobalyptica99@gmail.com>
2024-02-17 13:01:34 -0500

    really-good-weather

--- resources/script.js
+++ resources/script.js
@@ -1,5 +1,5 @@
 //state
-let currentCity = "Scranton";
+let currentCity = "New York";
 let units = "imperial";
 
 //selectors
@@ -9,6 +9,21 @@ let weather_forecast = document.querySelector(".weather_forecast");
 let weather_temperature = document.querySelector(".weather_temperature");
 let weather_icon = document.querySelector(".weather_icon");
 let weather_minmax = document.querySelector(".weather_minmax");
+let weather_realfeel = document.querySelector(".weather_realfeel");
+let weather_humidity = document.querySelector(".weather_humidity");
+let weather_wind = document.querySelector(".weather_wind");
+let weather_pressure = document.querySelector(".weather_pressure");
+
+//search
+document.querySelector(".weather_search").addEventListener("submit", e => {
+    let search = document.querySelector(".weather_searchform");
+    //prevent default reload action
+    e.preventDefault();
+    //change current city
+    currentCity = search.value;
+    //get weather forecast
+    getWeather();
+})
 
 function convertTimeStamp(timestamp, timezone){
     const convertTimezone = timezone / 3600;
@@ -45,8 +60,13 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${
     weather_forecast.innerHTML = `<p>${data.weather[0].main}`
     weather_temperature.innerHTML = `${data.main.temp.toFixed()}&#176`
     weather_icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`
-    weather_minmax.innerHTML = `<p>Min:${data.main.temp_min.toFixed()}&#176</p>
+    weather_minmax.innerHTML = `
+    <p>Min:${data.main.temp_min.toFixed()}&#176</p>
     <p>Max:${data.main.temp_max.toFixed()}&#176</p>`
+    weather_realfeel.innerHTML = `${data.main.feels_like.toFixed()}&#176`
+    weather_humidity.innerHTML = `${data.main.humidity}%`
+    weather_wind.innerHTML = `${data.wind.speed} f/s`
+    weather_pressure.innerHTML = `${data.main.pressure} hPa`
 })
 }
 
