294308acc493a259c62ecc93d53a6840449d2b8b
abobalypse <abobalyptica99@gmail.com>
2024-02-17 12:37:09 -0500

    Really-good-weather-app

---
+++ resources/script.js
@@ -0,0 +1,53 @@
+//state
+let currentCity = "Scranton";
+let units = "imperial";
+
+//selectors
+let city = document.querySelector(".weather_city");
+let datetime = document.querySelector(".weather_datetime");
+let weather_forecast = document.querySelector(".weather_forecast");
+let weather_temperature = document.querySelector(".weather_temperature");
+let weather_icon = document.querySelector(".weather_icon");
+let weather_minmax = document.querySelector(".weather_minmax");
+
+function convertTimeStamp(timestamp, timezone){
+    const convertTimezone = timezone / 3600;
+
+    const date = new Date(timestamp * 1000);
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timezone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(convertTimezone)}`,
+        hour12: true,
+    } 
+    return date.toLocaleString("en-US", options)
+}
+
+//convert country code to name
+function convertCountryCode(country){
+    let regionNames = new Intl.DisplayNames
+    (["en"], {type:"region"});
+    return regionNames.of(country);
+};
+
+function getWeather() {
+    const API_KEY ='c9cd4b47e898fc9aa9e94116e5618f0e'
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${API_KEY}&units=${units}`).then
+(res => res.json()).then(data => {
+    city.innerHTML = `${data.name},${convertCountryCode(data.sys.country)}`
+    datetime.innerHTML = convertTimeStamp(data.dt, data.timezone);
+    weather_forecast.innerHTML = `<p>${data.weather[0].main}`
+    weather_temperature.innerHTML = `${data.main.temp.toFixed()}&#176`
+    weather_icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`
+    weather_minmax.innerHTML = `<p>Min:${data.main.temp_min.toFixed()}&#176</p>
+    <p>Max:${data.main.temp_max.toFixed()}&#176</p>`
+})
+}
+
+document.body.addEventListener("load", getWeather())
